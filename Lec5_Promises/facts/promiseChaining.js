e9a035855e3734fef78e5ddb2acab543e596ba89
Sushant <sushantberiwal@gmail.com>
2021-02-11 21:04:36 +0530

    promise chaining code

---
+++ Lec5_Promises/facts/promiseChaining.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+// 1. Initial state of promise is pending !
+// 2. Promise<pending> => Promise<data> // Promise<error>
+// 3. then and catch are always called on pending promise
+// 4. then attaches callback function to pending promise known as scb
+// 4. catch attaches callback function to pp known as fcb
+// 5. then also gives us a pending promise known as thenKaPromise
+
+// async functions => callback use hoga guaranteed !
+// callback => async function not guaranteed !
+
+//chaining concept !
+
+let f1KaPromise = fs.promises.readFile("./f1.txt");
+
+f1KaPromise.then(function(data){
+    console.log(data+"");
+    let f2KaPromise = fs.promises.readFile("./f2.txt");
+    return f2KaPromise;
+})
+.then(function(data){
+    console.log(data+"");
+    let f3KaPromise = fs.promises.readFile("./f3.txt");
+    return f3KaPromise;
+})
+.then(function(data){
+    console.log(data+"")
+})
+
+
+
---
+++ Lec5_Promises/facts/test.js
@@ -0,0 +1,11 @@
+function sum(){
+    return 10;
+}
+
+
+// let val = sum()
+// let value = val+10;
+
+// let value = sum()+10;
+
+
