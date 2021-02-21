508fc3f96cf63d44dfc86e08c7ef2a73ad374369
Sushant <sushantberiwal@gmail.com>
2021-02-21 17:06:57 +0530

    modified instaLiker

--- Lec9_InstaLiker/login.js
+++ Lec9_InstaLiker/login.js
@@ -18,7 +18,7 @@ const { id, pw } = require("./credentials");
     await tab.waitForSelector('input[name="username"]' , {visible:true});
     await tab.type('input[name="username"]' , id);
     await tab.type('input[name="password"]' , pw);
-    await Promise.all( [ tab.waitForNavigation({waitUntil:"networkidle2"}), tab.click('.sqdOP.L3NKy.y3zKF') ] );
+    await Promise.all( [ tab.waitForNavigation({waitUntil:"networkidle2"}), tab.click('.sqdOP.L3NKy.y3zKF') ] ); // for login !!
     await tab.waitForSelector('.sqdOP.yWX7d.y3zKF' , {visible:true});
     await tab.click('.sqdOP.yWX7d.y3zKF');
     await tab.waitForSelector('.aOOlW.HoLwm' , {visible:true});
@@ -26,7 +26,7 @@ const { id, pw } = require("./credentials");
     await tab.waitForSelector('.XTCLo.x3qfX' , {visible:true});
     await tab.type('.XTCLo.x3qfX' , "pepper_pepcoding");
     await tab.waitForSelector('.-qQT3' , {visible:true});
-    await Promise.all( [ tab.waitForNavigation({waitUntil:"networkidle2"}), tab.click('.-qQT3') ] );
+    await Promise.all( [ tab.waitForNavigation({waitUntil:"networkidle2"}), tab.click('.-qQT3') ] ); // for going to pepper page !!
     await tab.waitForSelector('.eLAPa' , {visible:true});
     await tab.click('.eLAPa')
     for(let i=0 ; i<109 ; i++){
